f43ee0e70fb666323384d968ab863104b4a73c60
ITFangyz <2463185230@qq.com>
2022-08-10 19:50:24 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,20 @@
 // .vuepress/config.js
 module.exports = {
+    title: 'fyz-ui',
+    base: '/fyz-ui/',
     themeConfig: {
-      sidebar: [
-        '/',
-        '/componentsDocs/card'
-      ]
+        nav:[{
+            text: 'Home', link: '/'
+        },{
+            text:"Github", link: 'https://github.com/ITFangyz/fyz-ui'
+        },{
+            text:"Npm", link: 'https://www.npmjs.com/package/fyz-ui'
+        }
+
+        ],
+        sidebar: [
+            '/',
+            '/componentsDocs/card'
+        ]
     }
   }
--- docs/componentsDocs/card.md
+++ docs/componentsDocs/card.md
@@ -2,7 +2,7 @@
 卡片组件
 
 ### 示例
-<m-card imgSrc="/logo.png" summary="Vue"></m-card>
+<m-card imgSrc="/fyz-ui/logo.png" summary="Vue"></m-card>
 
 ### 代码
 ```html
